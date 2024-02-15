064e1b32aadf870fcd3149768f9cec9f7bda9d3e
rayfranc <rayfranciscoplay@gmail.com>
2024-02-15 11:15:04 -0500

    Subiendo cambios

--- src/controllers/user.routes.js
+++ src/controllers/user.routes.js
@@ -22,4 +22,23 @@ router.get("/api/users", async (req, res) => {
   );
 });
 
+router.post("/api/users/:id/exercises", async (request, response) => {
+  const { description: des, duration: dur, date: dat } = request.body;
+  try {
+    const user = await UserModel.findById(request.params.id);
+    if (!user) {
+      return response.status(404).json("No user with that id");
+    }
+    user.exercises.push({
+      description: des,
+      duration: dur,
+      date: Date.parse(dat),
+    });
+    const { username, _id, description, date, duration } = await user.save();
+    response.json({ _id, username, description, duration, date });
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
 module.exports = router;
